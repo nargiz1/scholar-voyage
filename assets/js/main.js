4c7e3d1c1b3cd23c4c3c1d116dfbe7bed4b68bbd
nargiz1 <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>
2024-02-12 22:20:01 +0400

    program details

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    loop: true,
+    autoplay: true,
+    slidesPerView: 2,
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true
+    },
+  });
